489b67178955900efc6f42b861be1f5ced701601
Subash Bose <subash.bose@siemens.com>
2022-05-10 14:55:22 +0530

    Added code

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -1,6 +1,8 @@
 using System;
 using Xunit;
 using Statistics;
+using Statistics.Services;
+using System.Collections.Generic;
 
 namespace Statistics.Test
 {
@@ -11,21 +13,24 @@ namespace Statistics.Test
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{1.5, 8.9, 3.2, 4.5});
+                new List<double>{1.5, 8.9, 3.2, 4.5});
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
+            Assert.True(Math.Abs(computedStats.average - 4.525) <= epsilon);
+            Assert.True(Math.Abs(computedStats.max - 8.9) <= epsilon);
+            Assert.True(Math.Abs(computedStats.min - 1.5) <= epsilon);
         }
         [Fact]
         public void ReportsNaNForEmptyInput()
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{});
+                new List<double>{});
             //All fields of computedStats (average, max, min) must be
             //Double.NaN (not-a-number), as described in
             //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
+            Assert.True(Double.NaN.Equals(computedStats.average));
+            Assert.True(Double.NaN.Equals(computedStats.min));
+            Assert.True(Double.NaN.Equals(computedStats.min));
         }
         [Fact]
         public void RaisesAlertsIfMaxIsMoreThanThreshold()
@@ -34,9 +39,9 @@ namespace Statistics.Test
             var ledAlert = new LEDAlert();
             IAlerter[] alerters = {emailAlert, ledAlert};
 
-            const float maxThreshold = 10.2;
+            const double maxThreshold = 10.2;
             var statsAlerter = new StatsAlerter(maxThreshold, alerters);
-            statsAlerter.checkAndAlert(new List<___>{0.2, 11.9, 4.3, 8.5});
+            statsAlerter.checkAndAlert(new List<double>{0.2, 11.9, 4.3, 8.5});
 
             Assert.True(emailAlert.emailSent);
             Assert.True(ledAlert.ledGlows);
---
+++ Statistics/Services/EmailAlert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistics.Services
+{
+    public class EmailAlert : IAlerter
+    {
+        public bool emailSent;
+
+        public EmailAlert()
+        {
+            emailSent = false;
+        }
+        public void Alert()
+        {
+            emailSent = true;
+        }
+    }
+}
